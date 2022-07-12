3db80776ab1db210d566607ea980783110d43701
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-07-12 02:49:16 -0700

    main file created

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File - main.h
+ * Desc: This file contain the prototypes of all 
+ * the functions in this project.
+ */
+
+int _printf(const char *format, ...);
+
+#endif

e69efe2fefbdf4837d8fb8cc0aacaff53143d86c
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-07-12 15:05:48 -0700

    file updated

--- print_bases.c
+++ print_bases.c
@@ -3,10 +3,11 @@
 /**
  * print_hex - prints a number in hexadecimal base,
  * in lowercase
- * @l: va_list arguments from _printf
- * @f: pointer to the struct flags that determines
+ * @l: The va_list arguments from _printf
+ * @f: A pointer to the struct flags that determines
  * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
+ * Description: the function calls convert()
+ * which in turns converts the input
  * number into the correct base and returns it as a string
  * Return: the number of char printed
  */
@@ -27,8 +28,8 @@ int print_hex(va_list l, flags_t *f)
 /**
  * print_hex_big - prints a number in hexadecimal base,
  * in uppercase
- * @l: va_list arguments from _printf
- * @f: pointer to the struct that determines
+ * @l: The va_list arguments from _printf
+ * @f: The pointer to the struct that determines
  * if a flag is passed to _printf
  * Description: the function calls convert() which in turns converts the input
  * number into the correct base and returns it as a string
@@ -50,7 +51,7 @@ int print_hex_big(va_list l, flags_t *f)
 
 /**
  * print_binary - prints a number in base 2
- * @l: va_list arguments from _printf
+ * @l: The va_list arguments from _printf
  * @f: pointer to the struct that determines
  * if a flag is passed to _printf
  * Description: the function calls convert() which in turns converts the input

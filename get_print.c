c4d2aadfa37c4b8d18ca8f8a95748883f4de2103
NeoDyln <dylankipkoech1@gmail.com>
2022-07-12 17:27:42 +0000

    Updates

--- get_print.c
+++ get_print.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- *  * get_print - selects the right printing function
- *   * depending on the conversion specifier passed to _printf
- *    * @s: character that holds the conversion specifier
- *     * Description: the function loops through the structs array
- *      * func_arr[] to find a match between the specifier passed to _printf
- *       * and the first element of the struct, and then the approriate
- *        * printing function
- *         * Return: a pointer to the matching printing function
- *          */
+ * get_print - selects the right printing function
+ * depending on the conversion specifier passed to _printf
+ * @s: character that holds the conversion specifier
+ * Description: the function loops through the structs array
+ * func_arr[] to find a match between the specifier passed to _printf
+ * and the first element of the struct, and then the approriate
+ * printing function
+ * Return: a pointer to the matching printing function
+ */
+
 int (*get_print(char s))(va_list, flags_t *)
 {
-	ph func_arr[] = 
-	{
+	ph func_arr[] = {
 		{'i', print_int},
 		{'s', print_string},
 		{'c', print_char},
